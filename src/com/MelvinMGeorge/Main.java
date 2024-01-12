aff38c6d2523a043c8b2e39be0c556658a537c04
Melvin M George <melvinmgeorge1234@gmail.com>
2024-01-13 02:22:48 +0530

    commit

--- src/com/MelvinMGeorge/Main.java
+++ src/com/MelvinMGeorge/Main.java
@@ -1,7 +1,11 @@
 package com.MelvinMGeorge;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
     }
 }
