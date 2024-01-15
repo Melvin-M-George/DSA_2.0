1272d8172d32e7cb4cfa997ce308141c07eb2a43
Melvin M George <melvinmgeorge1234@gmail.com>
2024-01-15 13:56:13 +0530

    primitives

---
+++ src/com/MelvinMGeorge/Primitives.java
@@ -0,0 +1,14 @@
+package com.MelvinMGeorge;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int roll_no = 64;
+        //String is not primitive
+        String name = "Melvin";
+        char letter = 'F';
+        float decimal = 4.56f;
+        double LargeDecimalNumbers = 24234234766.77;
+        long largeIntegerValues = 2345474783479749437L;
+        boolean check = true;
+    }
+}
