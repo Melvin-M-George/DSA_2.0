b3e8fa9e3e2dc7e902b7a46039957b85a9ddef7f
Melvin M George <melvinmgeorge1234@gmail.com>
2024-01-16 17:26:54 +0530

    conditionals

---
+++ src/com/MelvinMGeorge/Conditionals.java
@@ -0,0 +1,21 @@
+package com.MelvinMGeorge;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        /* syntax
+            if(boolean expression T or F){
+            //body
+            } else{
+
+            }
+
+        */
+        int salary = 25400;
+
+
+
+
+
+
+    }
+}
