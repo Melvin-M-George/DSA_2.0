5fd68e741273028061b8f0f1b44e1737b5928bfd
Melvin M George <melvinmgeorge1234@gmail.com>
2024-01-16 19:17:20 +0530

    loops

---
+++ src/com/MelvinMGeorge/loops.java
@@ -0,0 +1,12 @@
+package com.MelvinMGeorge;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        // for loop
+        for (int i=1;i<=5;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
