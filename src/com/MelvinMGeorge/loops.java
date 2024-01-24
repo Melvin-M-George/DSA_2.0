4f36e1e9812b7623e503cd3ab27f5c3ab95fa794
Melvin M George <melvinmgeorge1234@gmail.com>
2024-01-24 10:05:40 +0530

    loops

--- src/com/MelvinMGeorge/loops.java
+++ src/com/MelvinMGeorge/loops.java
@@ -11,11 +11,42 @@ public class loops {
 
         //print numbers from 1 to n
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i = 1;i<=n;i++){
+//            System.out.print(i+" ");
+//        }
 
-        for(int i = 1;i<=n;i++){
-            System.out.print(i+" ");
-        }
+        //while loop
+        /*
+            syntax:
+            while(condition){
+                //body
+            }
+        */
+//        int num = 1;
+//        while(num <= 5){
+//            System.out.print(num+ " ");
+//            num++;
+//        }
+
+        // do while
+        /*
+            syntax:
+            do{
+
+            } while();
+         */
+        int num = 1;
+        do{
+            System.out.print(num+" ");
+            num++;
+        }while (num<=5);
+
+
+
+
+
     }
 }
