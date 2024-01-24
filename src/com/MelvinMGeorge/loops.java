2cb3c9501134537a2898606805ec644309ad87fa
Melvin M George <melvinmgeorge1234@gmail.com>
2024-01-24 09:49:09 +0530

    loops

--- src/com/MelvinMGeorge/loops.java
+++ src/com/MelvinMGeorge/loops.java
@@ -5,7 +5,16 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
         // for loop
-        for (int i=1;i<=5;i++){
+//        for (int i=1;i<=5;i++){
+//            System.out.print(i+" ");
+//        }
+
+        //print numbers from 1 to n
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1;i<=n;i++){
             System.out.print(i+" ");
         }
     }
