9ad499d6b07ec31446fdb43c22805d0655ca5284
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-08 19:20:18 +0530

    Swap

--- src/com/MelvinMGeorge/Swap.java
+++ src/com/MelvinMGeorge/Swap.java
@@ -2,13 +2,18 @@ package com.MelvinMGeorge;
 
 public class Swap {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
 
+//        int temp = a;
+//        a = b;
+//        b = temp;
+        int a,b;
+        swap(10,20);
+        System.out.println(a+ " " + b);
+    }
+
+    static void swap(int a, int b){
         int temp = a;
         a = b;
         b = temp;
-
-        System.out.println(a+ " " + b);
     }
 }
