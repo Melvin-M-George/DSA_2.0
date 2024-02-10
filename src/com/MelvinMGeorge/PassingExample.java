8bd7e653281b497ceac97d458de471002fd9c734
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-10 09:43:20 +0530

    PassingExample

---
+++ src/com/MelvinMGeorge/PassingExample.java
@@ -0,0 +1,14 @@
+package com.MelvinMGeorge;
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String name = "Melvin";
+        greetMe(name);
+
+    }
+
+    private static void greetMe(String a) {
+        System.out.println(a);
+
+    }
+}
