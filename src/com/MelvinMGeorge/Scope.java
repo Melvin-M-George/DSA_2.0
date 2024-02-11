c132f98fecb77176b6f7e96481465cb252257eaf
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-11 19:18:27 +0530

    Scope

--- src/com/MelvinMGeorge/Scope.java
+++ src/com/MelvinMGeorge/Scope.java
@@ -6,8 +6,12 @@ public class Scope {
         int b = 20;
 
         {
-
+            //int a = 90; already initialized the block, cannot initialize again
+            // a = 99; Can change the value
+            int c = 99;
+            //Values that are initialized in the block, will remain in the block
         }
+        //System.out.println(c); c cannot be used outside the block
     }
 
     static void random(int marks){
