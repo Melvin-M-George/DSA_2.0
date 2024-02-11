c83cdf779d5a4d9271dbbb71f96ddf03f45dfa19
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-11 19:37:04 +0530

    VarArgs

---
+++ src/com/MelvinMGeorge/VarArgs.java
@@ -0,0 +1,13 @@
+package com.MelvinMGeorge;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun(1,2,3,4,5,6,7,8,9);
+    }
+
+    static void fun(int ...a){
+        System.out.println(Arrays.toString(a));
+    }
+}
