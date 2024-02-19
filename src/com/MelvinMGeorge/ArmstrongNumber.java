53feff8367579dbcc4cae595a34fac55d9bca5f2
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-19 19:31:12 +0530

    ArmstrongNumber

--- src/com/MelvinMGeorge/ArmstrongNumber.java
+++ src/com/MelvinMGeorge/ArmstrongNumber.java
@@ -4,19 +4,26 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        boolean ans = isArmstrong(n);
+//        System.out.println(ans);
+
+        for (int i = 100; i < 1000 ; i++) {
+            if(isArmstrong(i)){
+                System.out.print(i+ " ");
+            }
+        }
     }
 
     static boolean isArmstrong(int n){
+        int original = n;
         int sum = 0;
         while(n > 0){
             int rem = n % 10;
-            sum = sum + (rem * rem * rem);
             n = n / 10;
+            sum = sum + (rem * rem * rem);
         }
-        if(sum == n){
-            return
-        }
+        return sum==original;
     }
 }
