c38077fa2a99a1ac3321996141a693e1a824da8b
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-21 10:09:33 +0530

    arrays basic

---
+++ src/com/MelvinMGeorge/arrays_basic.java
@@ -0,0 +1,18 @@
+package com.MelvinMGeorge;
+
+public class arrays_basic {
+    public static void main(String[] args) {
+        //syntax
+        // datatype [] variable_name = new datatype [size];
+
+        //Store 5 roll numbers
+        //int [] roll_no = new int[5];
+        //int[] roll_no = {1,2,3,4,5};
+        int [] roll_no; // declaration of array, reference variable is defined in the stack
+        roll_no = new int[5];// initialization ; object is being created in the memory
+
+        String str = null;
+
+
+    }
+}
