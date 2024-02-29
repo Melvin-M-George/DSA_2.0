bfc39f43cd29c75c77b0e40310aaa3281cd8ed42
Melvin M George <melvinmgeorge1234@gmail.com>
2024-02-29 10:15:46 +0530

    ArrayListExample

--- src/com/MelvinMGeorge/ArrayListExample.java
+++ src/com/MelvinMGeorge/ArrayListExample.java
@@ -1,10 +1,28 @@
 package com.MelvinMGeorge;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExample {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         //Syntax
         ArrayList<Integer> list = new ArrayList<>();
+//        list.add(55);
+//        list.add(555);
+//        list.add(55555);
+//        System.out.println(list);
+//        System.out.println(list.contains(55));
+//        list.set(0, 99);
+//        System.out.println(list);
+//        list.remove(2);
+//        System.out.println(list);
+//        System.out.println(list.get(1));
+
+        //input
+        for (int i = 0; i < 5; i++) {
+            list.add(sc.nextInt());
+        }
+        System.out.println(list);
     }
 }
