e5215e492f89afc3aa3e00c0696a129d2cdac090
Melvin M George <melvinmgeorge1234@gmail.com>
2024-03-17 12:32:02 +0530

    MaxWealth

---
+++ src/com/MelvinMGeorge/MaxWealth.java
@@ -0,0 +1,17 @@
+package com.MelvinMGeorge;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+
+    }
+
+    static int maximumWealth(int[][] accounts) {
+
+        for (int person = 0; person < accounts.length; person++) {
+            int row_sum = 0;
+            for (int account = 0; account < accounts[person].length ; account++) {
+                row_sum = row_sum + accounts[person][acc];
+            }
+        }
+    }
+}
